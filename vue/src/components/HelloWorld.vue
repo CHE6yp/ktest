836db94e860bf9d1b0c6ef9601fde6d690aefe37
CHE6yp <che6yp@gmail.com>
2022-12-11 22:32:54 +0300

    feat: Table with pagination, sorting and filters

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,40 +1,127 @@
 <template>
+
+  <br>
+  <br>
+  <span>Всего позиций - {{totalItems}}</span>
+  <br>
+  <br>
+
+  <div v-if="pagination" class="pagination">
+    <button v-if="pagination['hydra:first']" @click="getData(pagination['hydra:first'])">First </button>
+    <button v-if="pagination['hydra:previous']" @click="getData(pagination['hydra:previous'])">Previous </button>
+    <span> Страница {{pagination.currentPage}} </span>
+    <button v-if="pagination['hydra:next']" @click="getData(pagination['hydra:next'])">Next </button>
+    <button v-if="pagination['hydra:last']" @click="getData(pagination['hydra:last'])">Last </button>
+  </div>  
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <table>
+      <tr>
+        <th >
+          <span @click="sort('id')">id {{sorting.id? ' ('+sorting.id+')':''}}</span></th>
+        <th >
+          <span @click="sort('name')">name {{sorting.name? ' ('+sorting.name+')':''}}</span> <br>
+          <input v-model="searchFilters.name" type="text" @input="search">
+        </th>
+        <th >
+          <span @click="sort('category')">category {{sorting.category? ' ('+sorting.category+')':''}}</span> <br>
+          <input v-model="searchFilters.category" type="text" @input="search">
+        </th>
+        <th >
+          <span @click="sort('description')">description {{sorting.description? ' ('+sorting.description+')':''}}</span> <br>
+          <input v-model="searchFilters.description" type="text" @input="search">
+        </th>
+        <th >
+          <span @click="sort('weight')">weight {{sorting.weight? ' ('+sorting.weight+')':''}}</span> <br>
+          <input v-model="searchFilters.weight" type="text" @input="search">
+        </th>
+      </tr>
+      <tr v-for="item in items" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.category}}</td>
+        <td>{{item.description}}</td>
+        <td>{{item.weight}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted () {
+    this.getData('/api/products?page=1');
+  },
+  data () {
+    return {
+      file: null,
+      response: null,
+      items: null,
+      sorting: {
+        id: null,
+        name: null,
+        category: null,
+        description: null,
+        weight: null,
+      },
+      searchFilters: {
+        name: "",
+        category: "",
+        description: "",
+        weight: "",
+      },
+      pagination: null,
+      totalItems: 0,
+    };
+  },
+  methods: {
+    search() {
+      this.buildQuery();
+    },
+    sort(param) {
+      for (const sortParam in this.sorting) {
+        if (param == sortParam) {
+          if (this.sorting[sortParam] == null) {
+            this.sorting[sortParam] = 'asc';
+          } else if (this.sorting[sortParam] == 'asc') {
+            this.sorting[sortParam] = 'desc';
+          } else {
+            this.sorting[sortParam] = null;
+          }
+        }
+      }
+      
+      this.buildQuery();
+    },
+    buildQuery() {
+      let queryArr = [];
+      for (const sortParam in this.sorting) {
+        if (this.sorting[sortParam]) queryArr.push("order["+sortParam+"]=" + this.sorting[sortParam]);  
+      }
+      for (const filter in this.searchFilters) {
+        if (this.searchFilters[filter]!="") queryArr.push(filter+'='+this.searchFilters[filter]);
+      }
+
+      let query = queryArr.join("&");
+
+      this.getData('/api/products?'+ query);
+    },
+    async getData(query) {
+      await axios
+        .get('http://localhost:8080'+query)
+        .then( (response) => {
+          this.response = response
+          this.items = this.response.data['hydra:member'];
+          this.pagination = this.response.data['hydra:view'];
+          this.pagination.currentPage = this.pagination['@id'].split('page=')[1];
+          this.totalItems = this.response.data['hydra:totalItems'];
+        });
+    }
   }
 }
 </script>
@@ -54,5 +141,8 @@ li {
 }
 a {
   color: #42b983;
+}
+tr:nth-child(even) {
+  background-color: #cfc;
 }
 </style>
